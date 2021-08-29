4d4fd22c701d5ff5f079426a735d081e6e6187bc
alayraza <50444174+alayraza@users.noreply.github.com>
2021-08-30 01:11:52 +0500

    Push
    Push

---
+++ scrap.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const cheerio = require('cherio');
+const func=async ()=>{
+    const url ='https://webscraper.io/test-sites/e-commerce/allinone'
+    const {data}= await axios({
+        method: "GET",
+        url: url
+    })
+    const $ = cheerio.load(data)
+    const sele = 'head > meta'
+    $(sele).each((parentIdx,parentElem)=>{
+        if(parentElem.attribs.property!=undefined && parentElem.attribs.property=="og:site_name" || parentElem.attribs.property=="og:title" || parentElem.attribs.property=="og:image" || parentElem.attribs.property=="og:description"){
+            console.log(parentElem.attribs.property," ",parentElem.attribs.content)
+        }
+    })
+}
+func()
